4b1b802d8507ad839fb42d839993652b34a2c716
ovalhudi <67082662+ovalhudi@users.noreply.github.com>
2024-06-09 20:47:36 +0700

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f4d92f6ddf5c2b1c03e45525cf31bd5b",
+const RESOURCES = {"flutter_bootstrap.js": "3027bc87232e233d0de475ff0ca8fb1c",
 "version.json": "16628ee5fbdbc7447be32045d2006944",
 "index.html": "34b8c045db6b0936b25103625a5dd59b",
 "/": "34b8c045db6b0936b25103625a5dd59b",
-"main.dart.js": "1d974e10942f39cc139a865d8bed9be1",
+"main.dart.js": "0099073de2c939cc3d770323dff19947",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "609d149c851760b6bc6038b4a9dfd164",
 "icons/Icon-192.png": "609d149c851760b6bc6038b4a9dfd164",
